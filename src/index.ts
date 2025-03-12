e8671e65f75fec4daebec7e13e826da4741c9453
seahal <seahal.com@gmail.com>
2025-03-12 23:53:06 +0900

    [misc] ...

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import { Hono } from 'hono'
+//
+// const app = new Hono()
+//
+// app.get('/', (c) => {
+//   return c.text('Hello Hono! 2')
+// })
+//
+// app.get('/security.txt', (c) => {
+//   return c.text('Contact: https://www.umaxica.com/report-vuln\n' +
+//       'Preferred-Languages: ja, en')
+// })
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono! 2')
+const app = new Hono({
+  getPath: (req) => req.url.replace(/^https?:\/([^?]+).*$/, '$1'),
 })
+
+app.get('/umaxica.app/', (c) => c.text('hello app'))
+app.get('/umaxica.com/', (c) => c.text('hello com'))
+app.get('/umaxica.org/', (c) => c.text('hello org'))
 
 export default app
