70642d5f50a624240ebaeb4a4116f35e0b712f79
seahal <seahal.com@gmail.com>
2025-03-14 04:01:53 +0900

    [update] added security tips.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Hono } from 'hono'
+import { secureHeaders } from 'hono/secure-headers'
+
 //
 // const app = new Hono()
 //
@@ -17,11 +19,18 @@ const app = new Hono({
 
 app.get('/umaxica.com/', (c) => c.text('hello com'))
 app.get('/umaxica.com/robots.txt', (c) => c.text('User-agent: *\nAllow: /'))
+app.get('/umaxica.com/sitemap.xml', (c) => c.text(''))
 
 app.get('/umaxica.app/', (c) => c.text('hello app'))
 app.get('/umaxica.app/robots.txt', (c) => c.text('User-agent: *\nAllow: /'))
+app.get('/umaxica.com/sitemap.xml', (c) => c.text(''))
 
 app.get('/umaxica.org/', (c) => c.text('hello org'))
 app.get('/umaxica.org/robots.txt', (c) => c.text('User-agent: *\nAllow: /'))
+app.get('/umaxica.org/sitemap.xml', (c) => c.text(''))
+
+app.get('/localhost:8787/', (c) => c.text('aaa'))
+
+app.use(secureHeaders());
 
 export default app
