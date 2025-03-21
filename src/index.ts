1132e638ae48b30611ec530bc79ac385f2d3a72f
seahal <seahal.com@gmail.com>
2025-03-21 14:37:47 +0900

    [update] set up github actions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono! 2')
+const app = new Hono({
+  getPath: (req) => req.url.replace(/^https?:\/([^?]+).*$/, '$1'),
 })
+
+app.get('/umaxica.com/', (c) => c.text('hello com'))
+app.get('/umaxica.app/', (c) => c.text('hello app'))
+app.get('/umaxica.org/', (c) => c.text('hello org'))
 
 export default app
