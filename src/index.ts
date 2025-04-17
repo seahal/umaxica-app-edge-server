27a1a415dc00d917d7502a6a1404f8b2d7a8935f
seahal <seahal@gmail.com>
2025-04-18 00:45:51 +0900

    [UPDATE] security update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
-import { Hono } from "hono";
-import { compress } from "hono/compress";
 import { languageDetector } from "hono/language";
+import { Hono } from "hono/quick";
+import { cors } from "hono/cors";
+import { compress } from "hono/compress";
+import { csrf } from "hono/csrf";
+import { timeout } from "hono/timeout";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+import { trimTrailingSlash } from "hono/trailing-slash";
 import { secureHeaders } from "hono/secure-headers";
 
-const app = new Hono({
-	getPath: (req) => req.url.replace(/^https?:\/([^?]+).*$/, "$1"),
-});
+// const app = new Hono({
+// 	getPath: (req) => req.url.replace(/^https?:\/([^?]+).*$/, "$1"),
+// });
+const app = new Hono({ strict: true });
 
 app.use(
 	languageDetector({
@@ -14,17 +21,42 @@ app.use(
 	}),
 );
 app.use(secureHeaders());
+app.use(cors());
 app.use(compress());
+app.use(csrf());
+app.use(timeout(2000));
+app.use(logger());
+app.use(prettyJSON());
+app.use(trimTrailingSlash());
 
-app.get("/umaxica.com/", (c) => c.text("hello com x"));
-app.get("/umaxica.app/", (c) => c.text("hello app x"));
-app.get("/umaxica.org/", (c) => c.text("hello org x"));
-app.notFound((c) => {
-	return c.text("Custom 404 Message", 404);
+// app.get("/jp.umaxica.com/", (c) => {
+// 	c.header('Content-Type', 'text/plain; charset=UTF-8')
+// 	return c.text("Welcome to jp.umaxica.app, the corporation page of Umaxica.")});
+// app.get("/jp.umaxica.app/", (c) => c.html("Welcome to jp.umaxica.app, the service page of Umaxica."));
+// app.get("/jp.umaxica.org/", (c) => c.html("Welcome to jp.umaxica.app, the staff's page of Umaxica."));
+// // app.notFound((c) => {
+// // 	return c.text("Custom 404 Message", 404);
+// // });
+// app.get("/localhost:8787/lang", (c) => {
+// 	const lang = c.get("language");
+// 	return c.text(`Hello! Your language is ${lang}`);
+// });
+app.get("/", (c) => {
+    c.header('Content-Type', 'text/html; charset=UTF-8')
+    return c.html(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>Hono App</title>
+      </head>
+      <body>
+        <p>Cloudflare</p>
+      </body>
+    </html>
+  `);
 });
-app.get("/localhost:8787/lang", (c) => {
-	const lang = c.get("language");
-	return c.text(`Hello! Your language is ${lang}`);
-});
+
 
 export default app;
+
